7c8199630aea810e953a133f0729324d87280c64
Joe Alcantara <joe@joealcantara.com>
2025-01-10 22:33:54 +0000

    created window

---
+++ code/main.py
@@ -0,0 +1,23 @@
+# Currently on 19:40
+
+import pygame
+
+# general setup
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Shooter')
+running = True
+
+while running:
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # Draw the game
+ 
+    display_surface.fill('RED')
+    pygame.display.update()
+
+pygame.quit()
