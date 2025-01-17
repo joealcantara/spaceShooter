d4a7760f33ada86eb23b3deb649eaab9091ad2e3
Joe Alcantara <joe@joealcantara.com>
2025-01-17 22:36:05 +0000

    progress to 42.23

--- code/main.py
+++ code/main.py
@@ -1,6 +1,10 @@
-# Currently on 19:40
+# Currently on 42:23
 
 import pygame
+import random
+import os
+
+os.chdir('/Users/joe/Documents/spaceShooter/')
 
 # general setup
 pygame.init()
@@ -9,6 +13,18 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Space Shooter')
 running = True
 
+# plain surface
+surf = pygame.Surface((100, 200))
+surf.fill('orange')
+x = 100
+
+# importing an image
+player_surf = pygame.image.load('images/player.png').convert_alpha()
+
+# place 20 stars
+star_surf = pygame.image.load('images/star.png').convert_alpha()
+star_positions = [(random.randint(1, WINDOW_WIDTH), random.randint(1, WINDOW_HEIGHT)) for i in range(20)]
+
 while running:
     # Event loop
     for event in pygame.event.get():
@@ -16,8 +32,12 @@ while running:
             running = False
 
     # Draw the game
- 
-    display_surface.fill('RED')
+    x += 0.1
+    display_surface.fill('dark grey')
+    
+    for star in star_positions:
+        display_surface.blit(star_surf, star)
+    display_surface.blit(player_surf, (x, 250))
     pygame.display.update()
 
 pygame.quit()
